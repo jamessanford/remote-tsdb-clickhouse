7bc8e5b435d12de2b322c2c945c247a6248d49f9
James Sanford <jsanfordgit@froop.com>
2026-09-25 10:15:55 -0700

    When subsampling, pick last value of bucket instead of max value.

--- internal/clickhouse/read.go
+++ internal/clickhouse/read.go
@@ -54,7 +54,7 @@ func (ch *ClickHouseAdapter) ReadRequest(ctx context.Context, req *prompb.ReadRe
 			timeField = fmt.Sprintf("toStartOfInterval(updated_at, INTERVAL %d second)", interval)
 		}
 
-		rows, err := ch.db.QueryContext(ctx, "SELECT metric_name, arraySort(labels) as slb, "+timeField+" AS t, max(value) as max_0 FROM "+ch.table+" WHERE "+sb.Where()+" GROUP BY metric_name, slb, t ORDER BY metric_name, slb, t", sb.Args()...)
+		rows, err := ch.db.QueryContext(ctx, "SELECT metric_name, arraySort(labels) as slb, "+timeField+" AS t, argMax(value, updated_at) as v FROM "+ch.table+" WHERE "+sb.Where()+" GROUP BY metric_name, slb, t ORDER BY metric_name, slb, t", sb.Args()...)
 		if err != nil {
 			return nil, err
 		}
